15b21837d3e9f1dc9f5e92f199951075380e51c2
sep <dserfezi@gmail.com>
2024-09-16 13:08:11 +0200

    misc_16_09_24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,10 +23,10 @@ import { StorageService } from './services/storage.service';
 import { EventsService } from './services/events.service';
 import { SerialLinkService } from './services/serial-link.service';
 import { UtilsService } from './services/utils.service';
-//import { UdpService } from './services/udp.service';
-//import { SerialPortService } from './services/serial-port.service';
+import { UdpService } from './services/udp.service';
+import { SerialPortService } from './services/serial-port.service';
 // test
-import { TestService } from './services/test.service';
+//import { TestService } from './services/test.service';
 // test
 
 import * as gConst from './gConst';
@@ -87,9 +87,9 @@ export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
         private httpClient: HttpClient,
         private utils: UtilsService,
         public serialLink: SerialLinkService,
-        //public udp: UdpService,
-        //public serial: SerialPortService,
-        public testService: TestService
+        public udp: UdpService,
+        public serial: SerialPortService,
+        //public testService: TestService
     ) {
         // ---
     }
@@ -156,8 +156,8 @@ export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
      */
     @HostListener('window:beforeunload')
     closeComms(){
-        //this.udp.closeSocket();
-        //this.serial.closeComPort();
+        this.udp.closeSocket();
+        this.serial.closeComPort();
     };
 
     /***********************************************************************************************
